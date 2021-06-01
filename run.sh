79c7d0c7361210774301517ad8d535a6db1aa5b4
Snockow6 <lostinspacefighter2@gmail.com>
2021-06-01 15:35:24 +1200

    Updated run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+mkdir -p /podget/logs
 while true
 do
-  runuser -l abc -c "/usr/bin/podget"
+  runuser -l abc -c "/usr/bin/podget" | tee -a /podget/logs/podget.log
   sleep $Refresh
 done
