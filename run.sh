05e295c999df771fc5ed2d9f9d10654b2e594a77
Snockow6 <lostinspacefighter2@gmail.com>
2021-06-02 21:49:37 +1200

    Update run.sh
    added date for log generation

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ usermod -u $PUID abc
 groupmod -g $PGID abc
 while true
 do
-  runuser -l abc -c "/usr/bin/podget" | tee -a /podget/logs/podget.log
+  runuser -l abc -c "/usr/bin/podget" | tee -a /podget/logs/podget-$(date +'%m/%d/%Y').log
+  echo waiting $Refresh | tee -a /podget/logs/podget-$(date +'%m/%d/%Y').log
   sleep $Refresh
 done
