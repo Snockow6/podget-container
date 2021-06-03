92bbc07c38e63f088977816e86bb476c4d065c8e
Snockow6 <lostinspacefighter2@gmail.com>
2021-06-03 13:11:19 +1200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-mkdir -p /podget/logs
+mkdir -p /Podget/logs
 usermod -u $PUID abc
 groupmod -g $PGID abc
+chown abc /Podget/logs
 while true
 do
-  runuser -l abc -c "/usr/bin/podget" | tee -a /podget/logs/podget-$(date +'%m/%d/%Y').log
-  echo waiting $Refresh | tee -a /podget/logs/podget-$(date +'%m/%d/%Y').log
+  runuser -l abc -c "/usr/bin/podget" | tee -a /Podget/logs/podget-$(date +'%m/%d/%Y').log
+  echo waiting $Refresh | tee -a /Podget/logs/podget-$(date +'%m/%d/%Y').log
   sleep $Refresh
 done
